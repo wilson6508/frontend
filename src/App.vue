eacaa7d5e627e7605d337f20da545e51d5a358bb
wilson6508 <wilson650837@gmail.com>
2022-03-07 00:30:00 +0800

    1

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,7 @@
     <div id="nav">
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
+      <!-- <div>123</div> -->
     </div>
     <router-view />
   </div>
